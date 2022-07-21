a89d60b381cffb068992f32ded51e3688e9c1a12
Pablo Corso <pabcorso@gmail.com>
2022-07-21 21:15:47 +0200

    Add favicons

--- app/root.tsx
+++ app/root.tsx
@@ -16,15 +16,43 @@ import {
 import tailwindStylesheetUrl from "./tailwind.css";
 import { getUser } from "./session.server";
 
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Easy Menu",
   viewport: "width=device-width,initial-scale=1",
+  "apple-mobile-web-app-title": "Easy Menu",
+  "application-name": "Easy Menu",
+  "theme-color": "#ffffff",
+  "msapplication-TileColor": "#2b5797",
 });
+
+export const links: LinksFunction = () => [
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicons/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicons/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicons/favicon-16x16.png",
+  },
+  {
+    rel: "mask-icon",
+    href: "/favicons/safari-pinned-tab.svg",
+    color: "#3466ef",
+  },
+  { rel: "manifest", href: "/site.webmanifest" },
+  { rel: "icon", href: "/favicon.ico" },
+  { rel: "stylesheet", href: tailwindStylesheetUrl },
+];
 
 type LoaderData = {
   user: Awaited<ReturnType<typeof getUser>>;
